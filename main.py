8ca5c892f05fbc61fe22a7b4f5738d1d96fa1b74
Praveen Bodasingi <praveen.bodasingi@realpage.com>
2024-02-27 17:23:46 +0530

    Changed SDK Key and Feature Flag Key

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from ldclient.config import Config
 
 # Set sdk_key to your LaunchDarkly SDK key before running
 # sdk_key = "sdk-78ede243-f829-4c0a-8ed1-2c0e92892246"
-sdk_key = "sdk-09738248-291d-4c1a-9aa2-4a9d077775ee"
+sdk_key = "sdk-3ddb7058-6c4f-457b-b3e9-2394ef4684a5"
 
 
 # Set feature_flag_key to the feature flag key you want to evaluate
-feature_flag_key = "contextAdd"
+feature_flag_key = "AdditionFlag"
 
 def show_message(s):
   print("*** %s" % s)
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
   # Set up the evaluation context. This context should appear on your LaunchDarkly contexts
   # dashboard soon after you run the demo.
-  context = Context.builder('test-user-key').name('Praveen').build()
+  context = Context.builder('Addition-user-key').name('Praveen').build()
 
   flag_value = ldclient.get().variation(feature_flag_key, context, False)
 
